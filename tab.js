c4d528a012618f9310a5061ad80e835602fa6433
simurai <simulus@gmail.com>
2021-06-11 14:55:26 +0900

    Update tab.js

--- tab.js
+++ tab.js
@@ -2,3 +2,6 @@ int main(){
     return 0; // 4 spaces
 	return 0; // 1 tab
 }
+
+<span class="h4 pl-3 v-align-middle" <%= test_selector "ghae-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header" %>><%= content_for(:bootstrap_heading) %></span>
+	<span class="h4 pl-3 v-align-middle" <%= test_selector "ghae-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header-bootstrap-header" %>><%= content_for(:bootstrap_heading) %></span>
